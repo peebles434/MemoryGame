b21ba94737b0c549299cf90dc0fc76264a02eb0a
Ryan Campbell <Rpcampbell434@aol.com>
2020-06-13 10:49:23 -0400

    Added temporary restart function based on page refresh

--- src/Components/GameDisplay.js
+++ src/Components/GameDisplay.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { observer } from "mobx-react";
 import { Stopwatch } from "./Stopwatch";
 import { Button } from "@material-ui/core";
 
 export const GameDisplay = () => {
+  // TODO: create restart handler that doesn't rely on page refresh
+  const tempRestartHandler = () => {
+    window.location.reload();
+  };
   return (
     <div className="gameDisplayBox noselect">
       <Stopwatch />
-      <Button className="restart" variant="contained" color="primary">
+      <Button
+        className="restart"
+        variant="contained"
+        color="primary"
+        onClick={tempRestartHandler}
+      >
         Restart
       </Button>
     </div>
